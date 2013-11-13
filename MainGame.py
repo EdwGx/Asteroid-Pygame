a6a49c4b39f46ba779d6fae760ff50ec139fea61
EdwGx <edguo98@gmail.com>
2013-11-13 17:12:37 -0500

    Coding

--- MainGame.py
+++ MainGame.py
@@ -3,6 +3,7 @@ import random
 
 from physic import*
 from color import*
+from sprite import*
 
 class MagBall(pygame.sprite.DirtySprite):
     def __init__ (self,number,color,mu,di):
@@ -49,26 +50,24 @@ class BadBall(MagBall):
     def __init__ (self):
         global bad_list
         rand = random.randrange(9)
+        randNum = random.randint(-5,5)
         if rand < 2:
-            color = green
             mu = False
             di = False
         elif rand < 6:
-            color = red
             mu = False
             di = False
         elif rand < 8:
-            sign = 2
-            color = green
             mu = True
             di = False
         else:
-            sign = 3
-            color = red
             mu = False
             di = True
-            
-        MagBall.__init__(self,random.randint(1,9),color,mu,di)
+        if (randNum < 0) or (di):
+            color = red
+        else:
+            color = green
+        MagBall.__init__(self,randNum,color,mu,di)
         startY = random.randrange(sizeY-60)
         self.moveSpeed = random.randrange(2)
         self.moveDir = random.randrange(2)
@@ -94,7 +93,10 @@ class PlayerBall(MagBall):
         self.rect.y = posY
         self.freeFall = True
     def update(self):
-        global sizeY
+        global sizeY,playerLosted
+        if self.number == 0:
+            playerLosted = True
+            print 'you lost'
         if self.freeFall:
             if (sizeY - self.radius*2)>self.rect.y : 
                 self.rect.y = freeFall(pygame.time.get_ticks()-self.fallBeginT,self.fallBeginY)
@@ -125,6 +127,19 @@ def draw_mouse():
                      (mX,mY+from_center),3)
     # center part
     pygame.draw.circle(screen,red,(mX,mY),3,0)
+
+'''def number_collide(spriteA,spriteB):
+    if ((spriteA.mu == False) and (spriteA.di == False)) and(
+        (spriteB.mu == False) and (spriteB.di == False)):
+            spriteA.number += spriteB.number
+    elif badBall.mu:
+        player.number = player.number*badBall.number
+    elif badBall.di:
+        if player.number%badBall.number == 0:
+            player.number = int(player.number/badBall.number)
+        else:
+            print ('not integer')
+    player.redraw()'''
 
 def jump_bar():
     global jumPower, moveY
@@ -159,9 +174,15 @@ jumPower = 1
 screen = pygame.display.set_mode([sizeX,sizeY])
 font = pygame.font.SysFont("comicsansms",30)
 pygame.display.set_caption("My Game")
+playerLosted = False
 
 #init player
 player = PlayerBall(1,200,400)
+player.add(full_list,ball_list)
+BadBall()
+player.add(full_list,ball_list)
+BadBall()
+BadBall()
 BadBall()
 moveY = False
 
@@ -205,20 +226,20 @@ while done == False:
     frames = 60
     #logic
     #coll_mouse_list = atom_list.get_sprites_at((mX,mY))
-
-    coll_bad_player = pygame.sprite.spritecollide(player,bad_list,True)
+    for badBall in bad_list:
+        for ballB in pygame.sprite.spritecollide(badBall,bad_list,False,pygame.sprite.collide_circle):
+            if not(badBall == ballB):
+                print 'killed'
+                number_collide(badBall,ballB,True,True)
+            
+        
+        coll_bad_player = pygame.sprite.spritecollide(player,bad_list,True,pygame.sprite.collide_circle)
     for badBall in coll_bad_player:
-        if badBall.mu == False and badBall.di == False:
-            player.number += badBall.number
-        else:
-            if badBall.mu:
-                player.number = player.number*badBall.number
-            elif badBall.di:
-                if player.number%badBall.number == 0:
-                    player.number = player.number/badBall.number
-                else:
-                    print 'not integer'
-        player.redraw()
+        kill_player = number_collide(player,badBall,False,True)
+        if kill_player:
+            playerLosted = True
+            print 'you lost'
+
                     
                     
                 
